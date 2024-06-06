1725781746c4a5699dcacddcaefead343a37b676
Nicholas Darren <117182669+Dritzz7@users.noreply.github.com>
2024-06-06 21:15:01 +0700

    tolong dicek yog

---
+++ addPatient.c
@@ -0,0 +1,57 @@
+//Nama : Nicholas Darren
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "struct.h"
+#include "parsingDate.c"
+#include "LinkedList.c"
+
+date parseDate(char* dateString, char* original);
+
+void addPatient(DataPasien** head) {
+    DataPasien* newNode = createDataPasienNode();
+
+    printf("Masukkan data pasien:\n");
+    
+    printf("Index: ");
+    scanf("%d", &newNode->index);
+
+    printf("Nama: ");
+    scanf(" %[^\n]s", newNode->nama);
+
+    printf("Alamat: ");
+    scanf(" %[^\n]s", newNode->alamat);
+
+    printf("Kota: ");
+    scanf(" %[^\n]s", newNode->kota);
+
+    printf("Tempat Lahir: ");
+    scanf(" %[^\n]s", newNode->tempatLahir);
+
+    printf("Tanggal Lahir (dd mmm yyyy): ");
+    char dateString[50];
+    fgets(dateString, sizeof(dateString), stdin);
+    dateString[strcspn(dateString, "\n")] = 0;
+    char original[20];
+    newNode->tgllahir = parseDate(dateString, original);
+
+    printf("Umur: ");
+    scanf("%d", &newNode->umur);
+
+    printf("BPJS: ");
+    scanf("%s", newNode->BPJS);
+
+    printf("Patient ID: ");
+    scanf("%s", newNode->patientID);
+
+    // Add pasien baru
+    insertDataPasienAtEnd(head, newNode); //fungsi ini pake yang dibuat El di linkedlist.c
+
+    printf("Pasien berhasil ditambahkan!\n");
+}
+
+int main(){
+    DataPasien* pasienHead = NULL;
+    addPatient(&pasienHead);
+}
