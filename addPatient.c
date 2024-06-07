f056ea2a7ddc0da930fc4200011f296b5bb131e1
Nicholas Darren <117182669+Dritzz7@users.noreply.github.com>
2024-06-07 18:25:24 +0700

    Update supaya user harus masukkin format yang acceptable

--- addPatient.c
+++ addPatient.c
@@ -9,15 +9,23 @@
 #include "parsingDate.c"
 #include "LinkedList.c"
 
-date parseDate(char* dateString, char* original); //Deklarasi fungsi
+date parseDate(char* dateString, char* original);
 
 void addPatient(DataPasien** head) {
     DataPasien* newNode = createDataPasienNode();
-
     printf("Masukkan data pasien:\n");
-    
-    printf("Index: ");
-    scanf("%d", &newNode->index);
+
+    DataPasien* temp = *head;
+    while (temp != NULL && temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    //Ini buat automatically assign index ke last index + 1
+    if (temp != NULL) {
+        newNode->index = temp->index + 1; // tambah 1 indexnya
+    } else {
+        newNode->index = 1; // Kalo linked list kosong, index =1
+    }
 
     printf("Nama: ");
     scanf(" %[^\n]s", newNode->nama);
@@ -31,12 +39,20 @@ void addPatient(DataPasien** head) {
     printf("Tempat Lahir: ");
     scanf(" %[^\n]s", newNode->tempatLahir);
 
-    printf("Tanggal Lahir (dd mmm yyyy): ");
     char dateString[50];
-    fgets(dateString, sizeof(dateString), stdin);
-    dateString[strcspn(dateString, "\n")] = 0;
-    char original[20];
-    newNode->tgllahir = parseDate(dateString, original);
+    int validDate = 0;
+    while (!validDate) { // Dibikin agar wajibin user masukin tanggal yang valid, kalo ngga di loop nanti programnya error
+        printf("Tanggal Lahir (dd mmm yyyy): ");
+        fgets(dateString, sizeof(dateString), stdin);
+        dateString[strcspn(dateString, "\n")] = 0;
+        char original[20];
+        newNode->tgllahir = parseDate(dateString, original);
+        if (newNode->tgllahir.date > 0 && newNode->tgllahir.month > 0 && newNode->tgllahir.year > 0) {
+            validDate = 1; // Jadi 1 and break the loop kalo valid
+        } else {
+            printf("Format tanggal tidak valid. Masukkan ulang.\n");
+        }
+    }
 
     printf("Umur: ");
     scanf("%d", &newNode->umur);
