a2305e62fad866ccc4cb89a1e95995c8205126ac
Yogium <mpp.yoga@gmail.com>
2024-06-09 20:09:10 +0700

    edited struct.h and created visit_manipulate.c with rudimentary functions for manipulating visit data.

--- visit_manipulate.c
+++ visit_manipulate.c
@@ -13,11 +13,12 @@ Deskripsi: fungsi untuk melakukan hal di bawah kepada data kunjungan:
 #include <string.h>
 #include "struct.h"
 
-DataKunjungan *createVisit(DataPasien *pasien, int diagnosis, int tindakan, date control){
+//Fungsi untuk membuat data kunjungan baru
+DataKunjungan* createVisit(int index, date tanggal, char *patientID, int diagnosis, int tindakan, date control){
     DataKunjungan *newVisit = (DataKunjungan*)malloc(sizeof(DataKunjungan));
-    newVisit->index = pasien->index;
-    newVisit->tanggal = control;
-    strcpy(newVisit->patientID, pasien->patientID);
+    newVisit->index = index;
+    newVisit->tanggal = tanggal;
+    strcpy(newVisit->patientID, patientID);
     newVisit->diagnosis = diagnosis;
     newVisit->tindakan = tindakan;
     newVisit->control = control;
@@ -25,6 +26,76 @@ DataKunjungan *createVisit(DataPasien *pasien, int diagnosis, int tindakan, date
     return newVisit;
 }
 
-void addVisit(DataKunjungan **head, DataPasien *pasien){
+//Fungsi untuk menambahkan data kunjungan ke linked list
+DataKunjungan* addVisit(DataKunjungan *head, DataKunjungan *newVisit){
+    DataKunjungan *temp = head;
+    if(head == NULL){
+        head = newVisit;
+    }else{
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newVisit;
+    }
+    return head;
+}
 
+DataKunjungan* searchVisit(DataKunjungan *head, char *patientID){
+    DataKunjungan *temp = head;
+    while(temp != NULL){
+        if(strcmp(temp->patientID, patientID) == 0){
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+DataKunjungan* deleteVisit(DataKunjungan *head, char *patientID){
+    DataKunjungan *temp = head;
+    DataKunjungan *prev = NULL;
+    while(temp != NULL){
+        if(strcmp(temp->patientID, patientID) == 0){
+            if(prev == NULL){
+                head = temp->next;
+            }else{
+                prev->next = temp->next;
+            }
+            free(temp);
+            return head;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+DataKunjungan* updateVisit(DataKunjungan *head, char *patientID, DataKunjungan *newVisit){
+    DataKunjungan *temp = head;
+    while(temp != NULL){
+        if(strcmp(temp->patientID, patientID) == 0){
+            temp->index = newVisit->index;
+            temp->tanggal = newVisit->tanggal;
+            strcpy(temp->patientID, newVisit->patientID);
+            temp->diagnosis = newVisit->diagnosis;
+            temp->tindakan = newVisit->tindakan;
+            temp->control = newVisit->control;
+            return head;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+void printVisit(DataKunjungan *head){
+    DataKunjungan *temp = head;
+    while(temp != NULL){
+        printf("Index: %d\n", temp->index);
+        printf("Tanggal: %d-%d-%d\n", temp->tanggal.date, temp->tanggal.month, temp->tanggal.year);
+        printf("Patient ID: %s\n", temp->patientID);
+        printf("Diagnosis: %s\n", penyakit[temp->diagnosis]);
+        printf("Tindakan: %s\n", tindakan[temp->tindakan].nama);
+        printf("Control: %d-%d-%d\n", temp->control.date, temp->control.month, temp->control.year);
+        temp = temp->next;
+    }
 }
