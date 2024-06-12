bac8e2bdf583c7daa1bb0b65a93e0fe0e529e548
elqonitat <161411539+elqonitat@users.noreply.github.com>
2024-06-12 15:58:28 +0700

    update nama aja

--- LinkedList.c
+++ LinkedList.c
@@ -1,6 +1,7 @@
-// Nama        : Nusaiba El Qonitat
-// Nama file   : LinkedList.c
-// Deskripsi   : Fungsi untuk menginput informasi dalam file csv ke dalam linked list
+/*  Nama      : Nusaiba El Qonitat
+    Nama file : LinkedList.c
+    Deskripsi : Fungsi untuk menginput informasi dalam file csv ke dalam linked list
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
